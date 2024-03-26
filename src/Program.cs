8ed9e7da908c67bb25d407c2d2f8fa3e967651aa
vdsrodrigo <vds.rodrigo@gmail.com>
2024-03-26 20:23:13 -0300

    Aplicando healthCheck e configurando a conexão dentro do appsettings da aplicação.

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,20 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<AcumuloContext>(options => options.UseNpgsql("Server=localhost;Database=postgres;Username=postgres;Password=postgres"));
+var configuration = builder.Configuration;
+builder.Services.AddDbContext<AcumuloContext>(options =>
+    options.UseNpgsql(configuration.GetSection("Database").GetValue<string>("ConnectionString")));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+builder.Services.AddHealthChecks()
+    .AddDbContextCheck<AcumuloContext>("PostgreSQL", HealthStatus.Unhealthy, new[] { "database" });
+
 var app = builder.Build();
+app.MapHealthChecks("/health");
 
 if (app.Environment.IsDevelopment())
 
@@ -19,43 +26,42 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.MapPost("/acumuladorIndividual", async (AcumuloContext db, int acumulo) =>
-{
-    db.Add(new ShellRepository { Nome = "Shell", Valor = acumulo, RowVersion = 1 });
-    await db.SaveChangesAsync();
+    {
+        db.Add(new ShellRepository { Nome = "Shell", Valor = acumulo, RowVersion = 1 });
+        await db.SaveChangesAsync();
 
-    return Results.NoContent();
-})
-.WithName("PostAcumuladorIndividualShell")
-.WithOpenApi();
+        return Results.NoContent();
+    })
+    .WithName("PostAcumuladorIndividualShell")
+    .WithOpenApi();
 
 app.MapPatch("/acumulador", async (AcumuloContext db, int acumulo) =>
-{
-    const int maximoDeTentativas = 20;
-    var totalDeConflitos = 0;
-    for (int tentativa = 0; tentativa < maximoDeTentativas; tentativa++)
     {
-        try
+        const int maximoDeTentativas = 20;
+        var totalDeConflitos = 0;
+        for (int tentativa = 0; tentativa < maximoDeTentativas; tentativa++)
         {
-            var shell = await db.AcumuladorShell.FirstOrDefaultAsync();
-            if (shell is null) return Results.NotFound();
+            try
+            {
+                var shell = await db.AcumuladorShell.FirstOrDefaultAsync();
+                if (shell is null) return Results.NotFound();
 
-            shell.Valor += acumulo;
-            shell.RowVersion += 1;
+                shell.Valor += acumulo;
+                shell.RowVersion += 1;
 
-            await db.SaveChangesAsync();
-            return Results.Ok($"Quantidade de conflitos ocorridas :{totalDeConflitos}");
+                await db.SaveChangesAsync();
+                return Results.Ok($"Quantidade de conflitos ocorridas :{totalDeConflitos}");
+            }
+            catch (DbUpdateConcurrencyException) when (tentativa < maximoDeTentativas - 1)
+            {
+                totalDeConflitos++;
+            }
         }
-        catch (DbUpdateConcurrencyException) when (tentativa < maximoDeTentativas - 1)
-        {
 
-            totalDeConflitos++;
-        }
-    }
-
-    Console.WriteLine(totalDeConflitos);
-    return Results.Ok($"Quantidade de conflitos ocorridas :{totalDeConflitos}");
-})
-.WithName("PatchAcumuladorShell")
-.WithOpenApi();
+        Console.WriteLine(totalDeConflitos);
+        return Results.Ok($"Quantidade de conflitos ocorridas :{totalDeConflitos}");
+    })
+    .WithName("PatchAcumuladorShell")
+    .WithOpenApi();
 
 app.Run();
